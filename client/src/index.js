cda64810dd0bb34202f6ab6976230a241dd0fa90
kaimikan <oihic@protonmail.com>
2022-05-16 23:09:20 +0200

    Add styling base

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import MainPage from "./component/MainPage";
+import "./styles/styles.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
